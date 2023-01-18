94137a8617b38fb4b47903825e52a0fdab93b2ce
thelogicguy <daniel_macdonald@yahoo.com>
2023-01-18 13:52:34 +0100

    Query1

---
+++ query-1.sql
@@ -0,0 +1,32 @@
+/* Query to display all tables in the employees_db*/
+SELECT * FROM information_schema.tables WHERE table_schema='public';
+
+/* Query to display the first 5 rows in the employees table*/
+SELECT *
+FROM employees
+LIMIT 5;
+
+/*1.) Display the minimum salary. */
+SELECT MIN(salary) AS min_salary
+FROM employees;
+
+/* 2.) Display the highest salary.*/
+SELECT MAX(salary) AS max_salary
+FROM employees;
+
+/*3.) Display the total salary of all employees.*/
+SELECT SUM(salary) AS total_salary
+FROM employees;
+
+/*4.) Display the average salary of all employees.*/
+SELECT ROUND(AVG(salary),2) AS avg_salary
+FROM employees;
+
+/* 5.) Issue a query to count the number of rows in the employee table. The result should be just one row. */
+SELECT COUNT(*) AS total_rows
+FROM employees;
+
+/* 6.) Issue a query to count the number of employees that make commission. The result should be just one row. */
+SELECT COUNT(*) AS emp_with_com
+FROM employees
+WHERE commission_pct != 0.0;
